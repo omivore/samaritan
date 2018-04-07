e7d86c84f3fb60131be643352ba42ed5f83c4863
Jason Tan <10.1.19.15.14.tan@gmail.com>
2018-04-07 02:56:22 -0400

    Create view with dynamic requests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+let test = {
+    search: 'College Park, MD',
+    requests: [
+        {
+            title: 'Help',
+            time: '10pm',
+            distance: '2mi away',
+            content: 'help I got hit by a hurricane come help me wash my car',
+            current: '2', total: '4'
+        },
+        {
+            title: 'Dan the Man',
+            time: '1am',
+            distance: 'Near you',
+            content: 'Need mah insomnia cookies man',
+            current: '0', total: '1'
+        },
+        {
+            title: '2',
+            time: '12pm',
+            distance: '5mi away',
+            content: 'oohhhh who lives in a pineapple under the seaaaa',
+            current: '1', total: '3'
+        }
+    ]
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', test);
 });
 
 module.exports = router;

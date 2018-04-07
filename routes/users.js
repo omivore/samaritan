4be867c8a6e0fc4c140036ec30d2dfffe30261c2
Jason Tan <10.1.19.15.14.tan@gmail.com>
2018-04-07 04:10:31 -0400

    Create a users page that can logout

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+function restrict(req, res, next) {
+    if (req.session.user) next();
+    else res.redirect('/login');
+}
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', restrict, function(req, res, next) {
+  res.render('users.pug', { username: req.session.user });
+});
+
+router.post('/', (req, res) => {
+    req.session.destroy(() => res.redirect('/'));
 });
 
 module.exports = router;

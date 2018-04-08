2825ebb5f1824cbfe24fe5de0e4ec55925e7b71e
Jason Tan <10.1.19.15.14.tan@gmail.com>
2018-04-08 00:24:13 -0400

    Give index page a default location

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(cookieParser())
 app.use(lessMiddleware(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.enable('trust proxy')
+
 db = require('./model/database').loadMongo() // promise
 
 app.use(session({
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,40 @@
 const express = require('express');
+const geoip = require('geoip-lite');        // Soon to be deprecated
 const database = require('../model/database');
 const geocode = require('../model/geocode');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    let coordinates, address;
     if (req.query.address) {
-        geocode.geocode(req.query.address, response => {
-            let location = {
-                coordinates: [
-                    Number(response.body[0].lat),
-                    Number(response.body[0].lon)
-                ],
+        coordinates = geocode.geocode(req.query.address, response =>
+            [Number(response.body[0].lat), Number(response.body[0].lon)]
+        );
+        address = address;
+    } else {
+        let geo = geoip.lookup(req.ip);
+        if (geoip.lookup(req.ip) != null) {
+            coordinates = Promise.resolve(geo.ll);
+            address = "Near you";
+        } else {
+            // Hardcoded UMD campus as the default location
+            coordinates = Promise.resolve([38.99203005, -76.9461029019905]);
+            address = "University of Maryland, College Park";
+        }
+    }
+
+    db.then(() => {
+        coordinates.then(coords => {
+            database.getRequests({
+                coordinates: coords,
                 maxDistance: 160000,      // TODO: make this dynamic
                 minDistance: 0
-            };
-            db.then(() => {
-                database.getRequests(location, req.query.address).then(data => {
-                    res.render('index', data);
-                })
-            });
+            }, address).then(data => {
+                res.render('index', data);
+            })
         });
-    } else {
-        // Default location?
-        res.render('index');
-    }
+    });
 });
 
 module.exports = router;

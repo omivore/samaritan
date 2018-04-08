c2adf1a23db862553b949d8ece6b2a552e3a8413
Dan Z <danny81299@gmail.com>
2018-04-07 23:53:56 -0400

    Add needed field in request flow and add help.js for help flow

---
+++ routes/help.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const database = require('../model/database');
+
+router.post('/', (req, res) => {
+    const uuid = req.body.reqId;
+    database.addVolunteer(uuid, req.session.user).then(data => console.log(data));
+    res.redirect('back');
+});
+
+module.exports = router;
+

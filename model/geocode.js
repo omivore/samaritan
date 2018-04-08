6c3534bd878a2542deefefcb80d3fc542b9a6627
Jason Tan <10.1.19.15.14.tan@gmail.com>
2018-04-08 01:19:00 -0400

    Change geocode to actually return a promise

--- model/geocode.js
+++ model/geocode.js
@@ -2,26 +2,36 @@ const unirest = require('unirest');
 
 module.exports = {
 
-    geocode: (search, callback) => {
-        return unirest.get("https://nominatim.openstreetmap.org/")
-            .headers({
-                'Accept': 'application/json',
-                'User-Agent': 'samaritan-node-client'
-            })
-            .query('format=jsonv2')
-            .query('q=' + search)
-            .end(response => callback(response));
+    geocode: search => {
+        return new Promise((resolve, reject) => {
+            unirest.get("https://nominatim.openstreetmap.org/")
+                .headers({
+                    'Accept': 'application/json',
+                    'User-Agent': 'samaritan-node-client'
+                })
+                .query('format=jsonv2')
+                .query('q=' + search)
+                .end(response => {
+                    if (response) resolve(response);
+                    else reject(response);
+                });
+        });
     },
 
-    reverse: (coordinates, callback) => {
-        return unirest.get("https://nominatim.openstreetmap.org/reverse")
-            .headers({
-                'Accept': 'application/json',
-                'User-Agent': 'samaritan-node-client'
-            })
-            .query('format=jsonv2')
-            .query({lat: coordinates[0]})
-            .query({lon: coordinates[1]})
-            .end(response => callback(response));
+    reverse: coordinates => {
+        return new Promise((resolve, reject) => {
+            unirest.get("https://nominatim.openstreetmap.org/reverse")
+                .headers({
+                    'Accept': 'application/json',
+                    'User-Agent': 'samaritan-node-client'
+                })
+                .query('format=jsonv2')
+                .query({lat: coordinates[0]})
+                .query({lon: coordinates[1]})
+                .end(response => {
+                    if (response) resolve(response);
+                    else reject(response);
+                });
+        });
     }
 };
